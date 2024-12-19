9181d85405ea98c1b183704cded501b0acee06d0
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-12-19 17:22:45 -0500

    partial, working on getting the linear fit to work using batch gradient descent

--- src/linear-classifier/main.c
+++ src/linear-classifier/main.c
@@ -56,33 +56,53 @@ int main (int argc, char * argv[]) {
     // ======================= INITIALIZE ARRAYS ========================== //
 
     Matrix * weights = matrix_create(no, 1 + nf);
+    weights->vals[0] = 50.0f;
+    weights->vals[1] = 0.4f;
+    weights->vals[2] = 2.0f;
+
+
     Matrix * predicted = matrix_create(no, ni);
     Matrix * error = matrix_create(no, ni);
+    Matrix * error_avg = matrix_create(no, 1);
+    Matrix * error_tr = matrix_create(ni, no);
+    Matrix * error_tr_bd = matrix_create(ni, 1 + nf);
+    Matrix * tmp = matrix_create(ni, 1 + nf);
     Matrix * gradient = matrix_create(no, 1 + nf);
     Matrix * step = matrix_create(no, 1 + nf);
 
-    size_t niters = 50;
-    float learning_rate = 0.000000001;
+    size_t niters = 50000000;
+    float learning_rate = 0.0000000001;
     for (size_t i = 0; i < niters; i++) {
         matrix_dotproduct(weights, features_td, predicted);
         matrix_subtract(predicted, labels_td, error);
-        matrix_print(stdout, error, "error");
-        matrix_dotproduct(error, features, gradient);
+        matrix_avg_per_row(error, error_avg);
+        matrix_print(stdout, error_avg, "error_avg");
+        matrix_transpose(error, error_tr);
+        matrix_broadcast_right(error_tr, error_tr_bd);
+        matrix_hadamardproduct(features, error_tr_bd, tmp);
+        matrix_acc_per_col(tmp, gradient);
         matrix_multiply_scalar(gradient, -1 * learning_rate, step);
         matrix_add_(weights, step);
     }
     matrix_print(stdout, weights, "weights");
+    matrix_print(stdout, error_avg, "error_avg");
 
     // =================== DEALLOCATE DYNAMIC MEMORY ====================== //
 
-    matrix_destroy(&weights);
-    matrix_destroy(&predicted);
-    matrix_destroy(&error);
     matrix_destroy(&step);
     matrix_destroy(&gradient);
+    matrix_destroy(&tmp);
+    matrix_destroy(&error_tr_bd);
+    matrix_destroy(&error_tr);
+    matrix_destroy(&error_avg);
+    matrix_destroy(&error);
+    matrix_destroy(&predicted);
+    matrix_destroy(&weights);
+    matrix_destroy(&labels_td);
     matrix_destroy(&labels);
     matrix_destroy(&features_td);
     matrix_destroy(&features);
+
 
     return EXIT_SUCCESS;
 }
