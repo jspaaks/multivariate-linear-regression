7e46c4c1dc2f8fb1247dd4e23d3795c1615b6707
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-09 09:59:20 -0500

    renamed Matrix member vals to xs

--- test/ops/test_ops.c
+++ test/ops/test_ops.c
@@ -5,21 +5,21 @@
 
 Test(ops, svm) {
     Matrix * scores = matrix_create(3, 3);
-    scores->vals[0] = 3.2f;
-    scores->vals[1] = 1.3f;
-    scores->vals[2] = 2.2f;
-    scores->vals[3] = 5.1f;
-    scores->vals[4] = 4.9f;
-    scores->vals[5] = 2.5f;
-    scores->vals[6] = -1.7f;
-    scores->vals[7] = 2.0f;
-    scores->vals[8] = -3.1f;
+    scores->xs[0] = 3.2f;
+    scores->xs[1] = 1.3f;
+    scores->xs[2] = 2.2f;
+    scores->xs[3] = 5.1f;
+    scores->xs[4] = 4.9f;
+    scores->xs[5] = 2.5f;
+    scores->xs[6] = -1.7f;
+    scores->xs[7] = 2.0f;
+    scores->xs[8] = -3.1f;
 
 
     Matrix * labels = matrix_create(1, 3);
-    labels->vals[0] = 0.0f;
-    labels->vals[1] = 1.0f;
-    labels->vals[2] = 2.0f;
+    labels->xs[0] = 0.0f;
+    labels->xs[1] = 1.0f;
+    labels->xs[2] = 2.0f;
 
     float expected = (2.9f + 0.0f + 12.9f) / 3;
     float lower = expected - 0.001f;
