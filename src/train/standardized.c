de988237646c1baf36e5febc896b0d980bcea20f
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-09 18:34:05 -0500

    (partial) harmonized the variable naming scheme with respect to which way is considered translated

--- src/train/standardized.c
+++ src/train/standardized.c
@@ -1,4 +1,5 @@
 #include "standardized.h"
+#include "plotting.h"
 #include <matrix/matrix.h>
 #include <assert.h>
 
@@ -33,7 +34,8 @@ void make_related_labels_matrices (Matrix * labels, Matrix * labels_zt, Matrix *
 }
 
 
-void run_standardized(size_t nsamples, size_t nfeatures, size_t nepochs, float learning_rate, const char * features_path, const char * labels_path) {
+void run_standardized(const size_t nsamples, const size_t nfeatures, const size_t nepochs, const float learning_rate,
+                      const char * features_path, const char * labels_path) {
     // suffixes z: standardized, t: transposed, 1: padded with ones
     Matrix * features = matrix_create(nsamples, nfeatures);
     Matrix * features_zt1 = matrix_create(1 + nfeatures, nsamples);
@@ -45,30 +47,34 @@ void run_standardized(size_t nsamples, size_t nfeatures, size_t nepochs, float l
     Matrix * labels_sigma = matrix_create(1, 1);
 
     Matrix * weights = matrix_create(1, 1 + nfeatures);
-    Matrix * predicted = matrix_create(1, nsamples);
-    Matrix * residuals = matrix_create(1, nsamples);
-    Matrix * residuals_bctdwn = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradients = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradient = matrix_create(1 + nfeatures, 1);
-    Matrix * step = matrix_create(1 + nfeatures, 1);
-    Matrix * step_t = matrix_create(1, 1 + nfeatures);
+    Matrix * predicted_t = matrix_create(1, nsamples);
+    Matrix * residuals_t = matrix_create(1, nsamples);
+    Matrix * residuals_tb = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradients_t = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradient_t = matrix_create(1 + nfeatures, 1);
+    Matrix * step_t = matrix_create(1 + nfeatures, 1);
+    Matrix * step = matrix_create(1, 1 + nfeatures);
+
+    Matrix * plotting_epochs = matrix_create(1 + nepochs, 1);
+    Matrix * plotting_sigma = matrix_create(1 + nepochs, 1);
 
     matrix_readxs(features_path, features);
-    {
-        size_t nrows = matrix_readnr(labels_path);
-        size_t nlabels = matrix_readnc(labels_path);
-        assert(nrows == nsamples && "Expected the number of rows in FEATURES to be "
-                                    "the same as the number of rows in LABELS");
-        assert(nlabels == 1 && "Expected the number of columns in LABELS to be 1");
-    }
-    matrix_readxs(labels_path, labels);
-
     make_related_features_matrices(features, features_zt1, features_mu, features_sigma);
-    make_related_labels_matrices(labels, labels_zt, labels_mu, labels_sigma);
-
+    matrix_print(stdout, "features", features);
     matrix_print(stdout, "features_zt1", features_zt1);
     matrix_print(stdout, "features_mu", features_mu);
     matrix_print(stdout, "features_sigma", features_sigma);
+
+    {
+        size_t nrows = matrix_readnr(labels_path);
+        size_t ncols = matrix_readnc(labels_path);
+        assert(nrows == nsamples && "Expected the number of rows in FEATURES to be "
+                                    "the same as the number of rows in LABELS");
+        assert(ncols == 1 && "Expected the number of columns in LABELS to be 1");
+    }
+    matrix_readxs(labels_path, labels);
+    make_related_labels_matrices(labels, labels_zt, labels_mu, labels_sigma);
+    matrix_print(stdout, "labels", labels);
     matrix_print(stdout, "labels_zt", labels_zt);
     matrix_print(stdout, "labels_mu", labels_mu);
     matrix_print(stdout, "labels_sigma", labels_sigma);
@@ -76,17 +82,22 @@ void run_standardized(size_t nsamples, size_t nfeatures, size_t nepochs, float l
     // ========================== ITERATION ============================== //
 
     for (size_t i = 0; i <= nepochs; i++) {
-        matrix_dotpro(weights, features_zt1, predicted);
-        matrix_ebesub(predicted, labels_zt, residuals);
-        matrix_bctdwn(residuals, residuals_bctdwn);
-        matrix_hadpro(residuals_bctdwn, features_zt1, gradients);
-        matrix_avgrgt(gradients, gradient);
-        matrix_scapro(gradient, learning_rate, step);
-        matrix_transp(step, step_t);
-        matrix_ebesub(weights, step_t, weights);
+        matrix_dotpro(weights, features_zt1, predicted_t);
+        matrix_ebesub(predicted_t, labels_zt, residuals_t);
+        {
+            plotting_epochs->xs[i] = i;
+            plotting_sigma->xs[i] = matrix_sdvall(residuals_t);
+        }
+        matrix_bctdwn(residuals_t, residuals_tb);
+        matrix_hadpro(residuals_tb, features_zt1, gradients_t);
+        matrix_avgrgt(gradients_t, gradient_t);
+        matrix_scapro(gradient_t, learning_rate, step_t);
+        matrix_transp(step_t, step);
+        matrix_ebesub(weights, step, weights);
     }
 
     matrix_print(stdout, "weights", weights);
+    plot_residuals("qtwidget", plotting_epochs, plotting_sigma, nepochs, nsamples);
 
     // =================== DEALLOCATE DYNAMIC MEMORY ====================== //
 
@@ -100,11 +111,14 @@ void run_standardized(size_t nsamples, size_t nfeatures, size_t nepochs, float l
     matrix_destroy(&labels_sigma);
 
     matrix_destroy(&weights);
-    matrix_destroy(&predicted);
-    matrix_destroy(&residuals);
-    matrix_destroy(&residuals_bctdwn);
-    matrix_destroy(&gradients);
-    matrix_destroy(&gradient);
-    matrix_destroy(&step);
+    matrix_destroy(&predicted_t);
+    matrix_destroy(&residuals_t);
+    matrix_destroy(&residuals_tb);
+    matrix_destroy(&gradients_t);
+    matrix_destroy(&gradient_t);
     matrix_destroy(&step_t);
+    matrix_destroy(&step);
+
+    matrix_destroy(&plotting_epochs);
+    matrix_destroy(&plotting_sigma);
 }
--- src/train/unstandardized.c
+++ src/train/unstandardized.c
@@ -1,4 +1,5 @@
 #include "unstandardized.h"
+#include "plotting.h"
 #include <matrix/matrix.h>
 #include <assert.h>
 
@@ -19,52 +20,63 @@ void make_related_features_matrices_unstandardized (Matrix * features, Matrix * 
 }
 
 
-void run_unstandardized(size_t nsamples, size_t nfeatures, size_t nepochs, float learning_rate, const char * features_path, const char * labels_path) {
-    // suffixes t: transposed, 1: padded with ones
+void run_unstandardized(const size_t nsamples, const size_t nfeatures, const size_t nepochs, const float learning_rate,
+                        const char * features_path, const char * labels_path) {
+    // matrix suffixes: t transposed, 1 padded with ones
     Matrix * features = matrix_create(nsamples, nfeatures);
     Matrix * features_t1 = matrix_create(1 + nfeatures, nsamples);
     Matrix * labels = matrix_create(nsamples, 1);
     Matrix * labels_t = matrix_create(1, nsamples);
 
     Matrix * weights = matrix_create(1, 1 + nfeatures);
-    Matrix * predicted = matrix_create(1, nsamples);
-    Matrix * residuals = matrix_create(1, nsamples);
-    Matrix * residuals_bctdwn = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradients = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradient = matrix_create(1 + nfeatures, 1);
-    Matrix * step = matrix_create(1 + nfeatures, 1);
-    Matrix * step_t = matrix_create(1, 1 + nfeatures);
+    Matrix * predicted_t = matrix_create(1, nsamples);
+    Matrix * residuals_t = matrix_create(1, nsamples);
+    Matrix * residuals_tb = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradients_t = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradient_t = matrix_create(1 + nfeatures, 1);
+    Matrix * step_t = matrix_create(1 + nfeatures, 1);
+    Matrix * step = matrix_create(1, 1 + nfeatures);
+
+    Matrix * plotting_epochs = matrix_create(1 + nepochs, 1);
+    Matrix * plotting_sigma = matrix_create(1 + nepochs, 1);
 
     matrix_readxs(features_path, features);
+    make_related_features_matrices_unstandardized(features, features_t1);
+    matrix_print(stdout, "features", features);
+    matrix_print(stdout, "features_t1", features_t1);
+
     {
         size_t nrows = matrix_readnr(labels_path);
-        size_t nlabels = matrix_readnc(labels_path);
+        size_t ncols = matrix_readnc(labels_path);
         assert(nrows == nsamples && "Expected the number of rows in FEATURES to be "
                                     "the same as the number of rows in LABELS");
-        assert(nlabels == 1 && "Expected the number of columns in LABELS to be 1");
+        assert(ncols == 1 && "Expected the number of columns in LABELS to be 1");
     }
     matrix_readxs(labels_path, labels);
-
-    make_related_features_matrices_unstandardized(features, features_t1);
     matrix_transp(labels, labels_t);
-
-    matrix_print(stdout, "features_t1", features_t1);
+    matrix_print(stdout, "labels", labels);
     matrix_print(stdout, "labels_t", labels_t);
 
     // ========================== ITERATION ============================== //
 
     for (size_t i = 0; i <= nepochs; i++) {
-        matrix_dotpro(weights, features_t1, predicted);
-        matrix_ebesub(predicted, labels_t, residuals);
-        matrix_bctdwn(residuals, residuals_bctdwn);
-        matrix_hadpro(residuals_bctdwn, features_t1, gradients);
-        matrix_avgrgt(gradients, gradient);
-        matrix_scapro(gradient, learning_rate, step);
-        matrix_transp(step, step_t);
-        matrix_ebesub(weights, step_t, weights);
+        matrix_dotpro(weights, features_t1, predicted_t);
+        matrix_ebesub(predicted_t, labels_t, residuals_t);
+        matrix_print(stdout, "weights", weights);
+        matrix_print(stdout, "residuals_t", residuals_t);
+        {
+            plotting_epochs->xs[i] = i;
+            plotting_sigma->xs[i] = matrix_sdvall(residuals_t);
+        }
+        matrix_bctdwn(residuals_t, residuals_tb);
+        matrix_hadpro(residuals_tb, features_t1, gradients_t);
+        matrix_avgrgt(gradients_t, gradient_t);
+        matrix_scapro(gradient_t, learning_rate, step_t);
+        matrix_transp(step_t, step);
+        matrix_ebesub(weights, step, weights);
     }
 
-    matrix_print(stdout, "weights", weights);
+    plot_residuals("qtwidget", plotting_epochs, plotting_sigma, nepochs, nsamples);
 
     // =================== DEALLOCATE DYNAMIC MEMORY ====================== //
 
@@ -74,11 +86,14 @@ void run_unstandardized(size_t nsamples, size_t nfeatures, size_t nepochs, float
     matrix_destroy(&labels_t);
 
     matrix_destroy(&weights);
-    matrix_destroy(&predicted);
-    matrix_destroy(&residuals);
-    matrix_destroy(&residuals_bctdwn);
-    matrix_destroy(&gradients);
-    matrix_destroy(&gradient);
-    matrix_destroy(&step);
+    matrix_destroy(&predicted_t);
+    matrix_destroy(&residuals_t);
+    matrix_destroy(&residuals_tb);
+    matrix_destroy(&gradients_t);
+    matrix_destroy(&gradient_t);
     matrix_destroy(&step_t);
+    matrix_destroy(&step);
+
+    matrix_destroy(&plotting_epochs);
+    matrix_destroy(&plotting_sigma);
 }
