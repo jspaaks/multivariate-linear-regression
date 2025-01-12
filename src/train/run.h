d076c584bfa5aac4764b8a9c72a07f51eea773d7
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-12 12:29:28 -0500

    reduced number of input arguments to function 'run' by using a struct

--- src/train/run.h
+++ src/train/run.h
@@ -2,7 +2,16 @@
 #define RUN_H
 #include <stddef.h>
 
-void run (size_t nsamples, size_t nfeatures, size_t nepochs, float learning_rate,
-          bool standardize, bool verbose, const char * features_path, const char * labels_path);
+
+struct inputs {
+    const size_t nepochs;
+    const float learning_rate;
+    const bool standardize;
+    const bool verbose;
+    const char * features_path;
+    const char * labels_path;
+};
+
+void run (struct inputs inputs);
 
 #endif
