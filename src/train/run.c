b261a1a301173a5a67fd11912c18e8b0e4bab868
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-12 12:30:51 -0500

    reduced number of input arguments to function 'run' by using a struct; updated the matrix suffixes used to differentiate between raw, transposed, padded, standardized, and broadcast data

--- src/train/run.c
+++ src/train/run.c
@@ -4,59 +4,98 @@
 #include <assert.h>
 
 
-void make_standardized_related_features_matrices (const Matrix * features_r, Matrix * features_z, Matrix * features_zt1, Matrix * features_mu, Matrix * features_sigma);
-void make_standardized_related_labels_matrices (const Matrix * labels_r, Matrix * labels_z, Matrix * labels_zt, Matrix * labels_mu, Matrix * labels_sigma);
-void make_unstandardized_related_features_matrices (const Matrix * features_r, Matrix * features_rt1);
-void make_unstandardized_related_labels_matrices (const Matrix * labels_r, Matrix * labels_rt);
+/*
+ * Matrix suffixes used herein:
+ *        _1    padded with ones
+ *        _b    broadcast
+ *        _raw  raw
+ *        _tr   transposed
+ *        _z    standardized
+ */
 
 
-void make_standardized_related_features_matrices (const Matrix * features_r, Matrix * features_z, Matrix * features_zt1, Matrix * features_mu, Matrix * features_sigma) {
-    size_t nsamples = features_r->nr;
-    size_t nfeatures = features_r->nc;
-    Matrix * features_zt = matrix_create(nfeatures, nsamples);
+float calc_halfssr (Matrix * residuals_tr);
+void make_standardized_related_features_matrices (const Matrix * features_raw, Matrix * features_z, Matrix * features_ztr1, Matrix * features_mu, Matrix * features_sigma);
+void make_standardized_related_labels_matrices (const Matrix * labels_raw, Matrix * labels_z, Matrix * labels_ztr, Matrix * labels_mu, Matrix * labels_sigma);
+void make_unstandardized_related_features_matrices (const Matrix * features_raw, Matrix * features_rawtr1);
+void make_unstandardized_related_labels_matrices (const Matrix * labels_raw, Matrix * labels_rawtr);
+
+
+float calc_halfssr (Matrix * residuals_tr) {
+    float halfssr = 0.0f;
+    for (size_t ic = 0; ic < residuals_tr->nc; ic++) {
+        float x = residuals_tr->xs[ic];
+        halfssr += 0.5 * x * x;
+    }
+    return halfssr;
+}
+
+
+void make_standardized_related_features_matrices (const Matrix * features_raw, Matrix * features_z, Matrix * features_ztr1, Matrix * features_mu, Matrix * features_sigma) {
+    size_t nsamples = features_raw->nr;
+    size_t nfeatures = features_raw->nc;
+    Matrix * features_ztr = matrix_create(nfeatures, nsamples);
     Matrix * ones = matrix_create(1, nsamples);
     matrix_ones(ones);
-    matrix_stzdwn(features_r, features_mu, features_sigma, features_z);
-    matrix_transp(features_z, features_zt);
-    matrix_vstack(ones, features_zt, features_zt1);
-    matrix_destroy(&features_zt);
+    matrix_stzdwn(features_raw, features_mu, features_sigma, features_z);
+    matrix_transp(features_z, features_ztr);
+    matrix_vstack(ones, features_ztr, features_ztr1);
+    matrix_destroy(&features_ztr);
     matrix_destroy(&ones);
 }
 
 
-void make_standardized_related_labels_matrices (const Matrix * labels_r, Matrix * labels_z, Matrix * labels_zt, Matrix * labels_mu, Matrix * labels_sigma) {
-    matrix_stzdwn(labels_r, labels_mu, labels_sigma, labels_z);
-    matrix_transp(labels_z, labels_zt);
+void make_standardized_related_labels_matrices (const Matrix * labels_raw, Matrix * labels_z, Matrix * labels_ztr, Matrix * labels_mu, Matrix * labels_sigma) {
+    matrix_stzdwn(labels_raw, labels_mu, labels_sigma, labels_z);
+    matrix_transp(labels_z, labels_ztr);
 }
 
 
-void make_unstandardized_related_features_matrices (const Matrix * features_r, Matrix * features_rt1) {
-    size_t nsamples = features_r->nr;
-    size_t nfeatures = features_r->nc;
+void make_unstandardized_related_features_matrices (const Matrix * features_raw, Matrix * features_rawtr1) {
+    size_t nsamples = features_raw->nr;
+    size_t nfeatures = features_raw->nc;
     Matrix * ones = matrix_create(1, nsamples);
-    Matrix * features_rt = matrix_create(nfeatures, nsamples);
+    Matrix * features_rawtr = matrix_create(nfeatures, nsamples);
     matrix_ones(ones);
-    matrix_transp(features_r, features_rt);
-    matrix_vstack(ones, features_rt, features_rt1);
+    matrix_transp(features_raw, features_rawtr);
+    matrix_vstack(ones, features_rawtr, features_rawtr1);
     matrix_destroy(&ones);
-    matrix_destroy(&features_rt);
+    matrix_destroy(&features_rawtr);
 }
 
 
-void make_unstandardized_related_labels_matrices (const Matrix * labels_r, Matrix * labels_rt) {
-    matrix_transp(labels_r, labels_rt);
+void make_unstandardized_related_labels_matrices (const Matrix * labels_raw, Matrix * labels_rawtr) {
+    matrix_transp(labels_raw, labels_rawtr);
 }
 
 
-void run (const size_t nsamples, const size_t nfeatures, const size_t nepochs, const float learning_rate,
-          const bool standardize, const bool verbose, const char * features_path, const char * labels_path) {
+void run (const struct inputs inputs) {
 
     fprintf(stderr, "Note: something is off with the backpropagation\n");
 
-    // matrix suffixes: r raw, t transposed, 1 padded with ones
-    Matrix * features_r = matrix_create(nsamples, nfeatures);
+    const size_t nepochs = inputs.nepochs;
+    const float learning_rate = inputs.learning_rate;
+    const bool standardize = inputs.standardize;
+    const bool verbose = inputs.verbose;
+    const char * features_path = inputs.features_path;
+    const char * labels_path = inputs.labels_path;
+    const size_t nsamples = matrix_readnr(features_path);
+    const size_t nfeatures = matrix_readnc(features_path);
+
+    if (verbose) {
+        fprintf(stdout, "features_path = %s\n", features_path);
+        fprintf(stdout, "labels_path = %s\n", labels_path);
+        fprintf(stdout, "learning_rate = %f\n", learning_rate);
+        fprintf(stdout, "nepochs = %zu\n", nepochs);
+        fprintf(stdout, "nfeatures = %zu\n", nfeatures);
+        fprintf(stdout, "nsamples = %zu\n", nsamples);
+        fprintf(stdout, "standardize = %s\n", standardize ? "true" : "false");
+        fprintf(stdout, "verbose = %s\n", verbose ? "true" : "false");
+    }
+
+    Matrix * features_raw = matrix_create(nsamples, nfeatures);
     Matrix * features = matrix_create(nsamples, nfeatures);
-    Matrix * features_t1 = matrix_create(1 + nfeatures, nsamples);
+    Matrix * features_tr1 = matrix_create(1 + nfeatures, nsamples);
     Matrix * features_mu = nullptr;
     Matrix * features_sigma = nullptr;
     if (standardize) {
@@ -64,9 +103,9 @@ void run (const size_t nsamples, const size_t nfeatures, const size_t nepochs, c
         features_sigma = matrix_create(1, nfeatures);
     }
 
-    Matrix * labels_r = matrix_create(nsamples, 1);
+    Matrix * labels_raw = matrix_create(nsamples, 1);
     Matrix * labels = matrix_create(nsamples, 1);
-    Matrix * labels_t = matrix_create(1, nsamples);
+    Matrix * labels_tr = matrix_create(1, nsamples);
     Matrix * labels_mu = nullptr;
     Matrix * labels_sigma = nullptr;
     if (standardize) {
@@ -75,20 +114,20 @@ void run (const size_t nsamples, const size_t nfeatures, const size_t nepochs, c
     }
 
     Matrix * weights = matrix_create(1, 1 + nfeatures);
-    Matrix * predicted_t = matrix_create(1, nsamples);
-    Matrix * residuals_t = matrix_create(1, nsamples);
-    Matrix * residuals_tb = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradients_t = matrix_create(1 + nfeatures, nsamples);
-    Matrix * gradient_t = matrix_create(1 + nfeatures, 1);
-    Matrix * step_t = matrix_create(1 + nfeatures, 1);
+    Matrix * predicted_tr = matrix_create(1, nsamples);
+    Matrix * residuals_tr = matrix_create(1, nsamples);
+    Matrix * residuals_trb = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradients_tr = matrix_create(1 + nfeatures, nsamples);
+    Matrix * gradient_tr = matrix_create(1 + nfeatures, 1);
+    Matrix * step_tr = matrix_create(1 + nfeatures, 1);
     Matrix * step = matrix_create(1, 1 + nfeatures);
 
-    Matrix * plotting_epochs = matrix_create(1 + nepochs, 1);
-    Matrix * plotting_sigma = matrix_create(1 + nepochs, 1);
+    Matrix * epochs = matrix_create(1 + nepochs, 1);
+    Matrix * losses = matrix_create(1 + nepochs, 1);
 
     // =====================  POPULATE DATA  ========================= //
 
-    matrix_readxs(features_path, features_r);
+    matrix_readxs(features_path, features_raw);
     {
         size_t nrows = matrix_readnr(labels_path);
         size_t ncols = matrix_readnc(labels_path);
@@ -96,21 +135,21 @@ void run (const size_t nsamples, const size_t nfeatures, const size_t nepochs, c
                                     "the same as the number of rows in LABELS");
         assert(ncols == 1 && "Expected the number of columns in LABELS to be 1");
     }
-    matrix_readxs(labels_path, labels_r);
+    matrix_readxs(labels_path, labels_raw);
 
     if (standardize) {
-        make_standardized_related_features_matrices(features_r, features, features_t1, features_mu, features_sigma);
-        make_standardized_related_labels_matrices(labels_r, labels, labels_t, labels_mu, labels_sigma);
+        make_standardized_related_features_matrices(features_raw, features, features_tr1, features_mu, features_sigma);
+        make_standardized_related_labels_matrices(labels_raw, labels, labels_tr, labels_mu, labels_sigma);
     } else {
-        make_unstandardized_related_features_matrices(features_r, features_t1);
-        make_unstandardized_related_labels_matrices(labels_r, labels_t);
+        make_unstandardized_related_features_matrices(features_raw, features_tr1);
+        make_unstandardized_related_labels_matrices(labels_raw, labels_tr);
     }
-    if (verbose) matrix_print(stdout, "features_r", features_r);
+    if (verbose) matrix_print(stdout, "features_raw", features_raw);
     if (verbose) matrix_print(stdout, "features", features);
-    if (verbose) matrix_print(stdout, "features_t1", features_t1);
-    if (verbose) matrix_print(stdout, "labels_r", labels_r);
+    if (verbose) matrix_print(stdout, "features_tr1", features_tr1);
+    if (verbose) matrix_print(stdout, "labels_raw", labels_raw);
     if (verbose) matrix_print(stdout, "labels", labels);
-    if (verbose) matrix_print(stdout, "labels_t", labels_t);
+    if (verbose) matrix_print(stdout, "labels_tr", labels_tr);
 
     // ========================== ITERATION ============================== //
 
@@ -118,70 +157,64 @@ void run (const size_t nsamples, const size_t nfeatures, const size_t nepochs, c
 
     for (size_t i = 0; i <= nepochs; i++) {
         if (verbose) fprintf(stdout, "+++++++++++++++++++++++++      epoch = %-5zu +++++++++++++++++++++++++\n", i);
-        matrix_dotpro(weights, features_t1, predicted_t);
+        matrix_dotpro(weights, features_tr1, predicted_tr);
         if (verbose) matrix_print(stdout, "weights", weights);
-        if (verbose) matrix_print(stdout, "features_t1", features_t1);
-        if (verbose) matrix_print(stdout, "predicted_t", predicted_t);
+        if (verbose) matrix_print(stdout, "features_tr1", features_tr1);
+        if (verbose) matrix_print(stdout, "predicted_tr", predicted_tr);
 
-        matrix_ebesub(predicted_t, labels_t, residuals_t);
-        if (verbose) matrix_print(stdout, "residuals_t", residuals_t);
+        matrix_ebesub(predicted_tr, labels_tr, residuals_tr);
+        if (verbose) matrix_print(stdout, "residuals_tr", residuals_tr);
 
-        {
-            float halfssr = 0.0f;
-            for (size_t ic = 0; ic < residuals_t->nc; ic++) {
-                float x = residuals_t->xs[ic];
-                halfssr += 0.5 * x * x;
-            }
-            plotting_epochs->xs[i] = i;
-            plotting_sigma->xs[i] = halfssr;
-        }
-        matrix_bctdwn(residuals_t, residuals_tb);
-        if (verbose) matrix_print(stdout, "residuals_tb", residuals_tb);
+        epochs->xs[i] = (float) i;
+        losses->xs[i] = calc_halfssr(residuals_tr);
 
-        matrix_hadpro(residuals_tb, features_t1, gradients_t);
-        if (verbose) matrix_print(stdout, "gradients_t", gradients_t);
+        matrix_bctdwn(residuals_tr, residuals_trb);
+        if (verbose) matrix_print(stdout, "residuals_trb", residuals_trb);
 
-        matrix_accrgt(gradients_t, gradient_t);
-        if (verbose) matrix_print(stdout, "gradient_t", gradient_t);
+        matrix_hadpro(residuals_trb, features_tr1, gradients_tr);
+        if (verbose) matrix_print(stdout, "gradients_tr", gradients_tr);
 
-        matrix_scapro(gradient_t, learning_rate, step_t);
-        if (verbose) matrix_print(stdout, "step_t", step_t);
+        matrix_accrgt(gradients_tr, gradient_tr);
+        if (verbose) matrix_print(stdout, "gradient_tr", gradient_tr);
 
-        matrix_transp(step_t, step);
+        matrix_scapro(gradient_tr, learning_rate, step_tr);
+        if (verbose) matrix_print(stdout, "step_tr", step_tr);
+
+        matrix_transp(step_tr, step);
         if (verbose) matrix_print(stdout, "step", step);
 
         matrix_ebesub(weights, step, weights);
     }
 
-    plot_residuals("qtwidget", plotting_epochs, plotting_sigma, nepochs, nsamples);
+    plot_residuals("qtwidget", nepochs, epochs, losses, nsamples);
 
     // =================== DEALLOCATE DYNAMIC MEMORY ====================== //
 
-    matrix_destroy(&features_r);
+    matrix_destroy(&features_raw);
     matrix_destroy(&features);
-    matrix_destroy(&features_t1);
+    matrix_destroy(&features_tr1);
     if (standardize) {
         matrix_destroy(&features_mu);
         matrix_destroy(&features_sigma);
     }
 
-    matrix_destroy(&labels_r);
+    matrix_destroy(&labels_raw);
     matrix_destroy(&labels);
-    matrix_destroy(&labels_t);
+    matrix_destroy(&labels_tr);
     if (standardize) {
         matrix_destroy(&labels_mu);
         matrix_destroy(&labels_sigma);
     }
 
     matrix_destroy(&weights);
-    matrix_destroy(&predicted_t);
-    matrix_destroy(&residuals_t);
-    matrix_destroy(&residuals_tb);
-    matrix_destroy(&gradients_t);
-    matrix_destroy(&gradient_t);
-    matrix_destroy(&step_t);
+    matrix_destroy(&predicted_tr);
+    matrix_destroy(&residuals_tr);
+    matrix_destroy(&residuals_trb);
+    matrix_destroy(&gradients_tr);
+    matrix_destroy(&gradient_tr);
+    matrix_destroy(&step_tr);
     matrix_destroy(&step);
 
-    matrix_destroy(&plotting_epochs);
-    matrix_destroy(&plotting_sigma);
+    matrix_destroy(&epochs);
+    matrix_destroy(&losses);
 }
