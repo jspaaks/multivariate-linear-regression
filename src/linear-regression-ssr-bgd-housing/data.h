0287269fb5b9c6a78e7b631dc1d1d1274bc031ce
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-14 13:21:15 -0500

    removed superfluous directory 'linear-regression-ssr-bgd-housing', has been superseded by 'mkdata' and 'train' executables

--- src/linear-regression-ssr-bgd-housing/data.h
+++
@@ -1,8 +0,0 @@
-#ifndef DATA_H
-#define DATA_H
-#include "matrix/matrix.h"
-
-void populate_features (const char * path, Matrix * features_raw);
-void populate_labels (const char * path, Matrix * labels_raw_transp);
-
-#endif
--- src/linear-regression-ssr-bgd-housing/plotting.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLOTTING_H
-#define PLOTTING_H
-#include "matrix/matrix.h"
-#include <plplot/plplot.h>
-
-void plot_residuals (PLCHAR_VECTOR device, const Matrix * iterations, const Matrix * avg_residuals, size_t niters, size_t ni);
-
-#endif
