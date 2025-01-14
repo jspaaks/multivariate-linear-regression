a44bfac9cfe02667cd17b1c7b1078137bed726f3
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-14 11:47:32 -0500

    removed superfluous directory src/linear-regression-ssr-bgd-artificial, is superseded by more flexible 'mkdata' and 'train'

--- src/linear-regression-ssr-bgd-artificial/data.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATA_H
-#define DATA_H
-#include "matrix/matrix.h"
-
-void populate_features (Matrix * features);
-void populate_labels (const Matrix * true_weights, const Matrix * features_transp, Matrix * labels_transp, Matrix * true_residuals_transp);
-void populate_true_weights (Matrix * true_weights);
-
-#endif
