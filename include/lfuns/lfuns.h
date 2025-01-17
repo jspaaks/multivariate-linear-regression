6b59830c09012d55968f1d5ee69186b555f35162
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-01-17 11:07:55 -0500

    cleanup of deprecated files and directories

--- include/lfuns/lfuns.h
+++
@@ -1,8 +0,0 @@
-#ifndef LFUNS_H
-#define LFUNS_H
-#include <stddef.h>
-
-float lfuns_softmax(const size_t n, const float * values, const size_t m);
-float lfuns_svm(const size_t n, const float * values, const size_t m);
-
-#endif
--- include/rfuns/rfuns.h
+++
@@ -1,8 +0,0 @@
-#ifndef LFUNS_H
-#define LFUNS_H
-#include <stddef.h>
-
-float rfuns_ell_1 (size_t n, float * weights);
-float rfuns_ell_2 (size_t n, float * weights);
-
-#endif
